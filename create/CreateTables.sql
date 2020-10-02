913dfe7a0232efac7c3530f9675cd3927044a2ff
Kacper <kacper.plusaa@gmail.com>
2020-10-02 23:17:44 +0200

    Create files modified

--- create/CreateTables.sql
+++ create/CreateTables.sql
@@ -1,24 +1,24 @@
 
-  create table [stoa].[dbo].[Tag]
+  create table [stoa_db].[dbo].[Tag]
   (
   id int primary KEY IDENTITY (1,1),
   name varchar(255) not null,
   )
-  create table [stoa].[dbo].[Task]
+  create table [stoa_db].[dbo].[Task]
   (
   id int primary KEY IDENTITY (1,1),
   name varchar(255) not null,
   priority int not null,
   Tag_id int not null,
-  Foreign key (Tag_id) references [stoa].[dbo].[Tag] (id)
+  Foreign key (Tag_id) references [stoa_db].[dbo].[Tag] (id)
   )
-  create table [stoa].[dbo].[Token]
+  create table [stoa_db].[dbo].[Token]
   (
   id int primary KEY IDENTITY (1,1),
   name varchar(255) not null,
   generated date not null,
   Task_id int,
-  Foreign key (Task_id) references [stoa].[dbo].[Task] (id)
+  Foreign key (Task_id) references [stoa_db].[dbo].[Task] (id)
   )
 
   
